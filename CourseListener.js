0ca46cabd522d836a5482bda5f6889df46327639
Glitch (glitch-hello-website) <none>
2023-04-10 04:50:34 +0000

    🎂📡 Checkpoint ./CourseListener.js:56626904/81472

--- CourseListener.js
+++ CourseListener.js
@@ -23,116 +23,217 @@ class CourseListener {
     constructor(participantID) {
         this.participantID = participantID;
         this.sender_psid = participantID.psid;
-        this.token = this.participantID.vle_accounts[0]
+        this.token = this.participantID.vle_accounts[0];
     }
-
     async listen() {
 
+        console.log('test');
 
-        let storedCoursesActivities;
-        console.log('test')
+        let storedCourseList;
 
         setInterval(async () => {
-            //console.log(await this.getCourses(await this.participantID.vle_accounts[0]));
-            //console.log(this.participantID.vle_accounts);
-            const courses = await this.getCourses(await this.participantID.vle_accounts[0])
-                .then(res => res)
-                .catch(err => 'COURSES ERROR');
 
+            const courseList = await this.getCourses(this.token)
+                .then((c) => c);
 
-            if (await courses) {
-                const courseActs = await Promise.all(courses.map(async (course) => {
-                    const activities = await this.getCourseWork(await this.token, await course.id).then(acts => acts);
-
-                    try {
-                        return {
-                            course: await course.name,
-                            activities: (await activities) ? await activities.map((act) => act.title) : []
-                        }
-                    } catch (err) {
-                        console.log(err)
-                    }
-                }));
-
-                if (await !storedCoursesActivities) {
-                    storedCoursesActivities = await courseActs
+            const courseWorksObj = await Promise.all(courseList.map(async (course) => {
+                try {
+                    const courseWork = await this.getCourseWorks(this.token, course.id);
+                    return {
+                        courseName: course.name,
+                        coursework: courseWork
+                    };
+                } catch (err) {
+                    console.log(err);
+                    return null;
                 }
+            }));
 
-                //console.log(courseActs)
-                //console.log("STORED:")
-                //console.log(storedCoursesActivities)
-                //console.log("READ:")
-                //console.log(courseActs);
+            if (!storedCourseList) storedCourseList = await courseList;
 
-                if (await courseActs) {
-                    // checks if course list length stayed the same
-                    if (courseActs.length == await storedCoursesActivities.length) {
+            if (await courseList && await storedCourseList && await courseWorksObj) {
+                let storedCourseWork;
+
+                if (storedCourseList.length === await courseList.length) {
 
 
-                        let storedActs;
-                        // for every courseAct
-                        courseActs.forEach(courseAct => {
-                            console.log("FOR EACH COURSE ACT:")
-                            // for every storedAct
-                            storedCoursesActivities.forEach(async storedAct => {
-                                //await callSendAPI(this.sender_psid, {text:"Scanning"})
-                                console.log("FOR EACH STORED ACT")
-                            })
-                            /*
-                            if (courseAct) {
-                                console.log('------------------------------')
-                                console.log(courseAct.course)
-                                console.log(courseAct.activities)
-                            }
-                            */
-                        })
+                    await courseList.map(async course => {
+                        let reqCourseWork = courseWorksObj.filter(obj => obj.courseName == course.name)
+                            .map(course => {
+                                if (course.coursework) {
+                                    const ret = course.coursework.map(work => work)
+                                    return ret.map(r => r)
+                                } else {
+                                    return []
+                                }
+                            }).map(course => course.map(c => c.title))
+                        reqCourseWork = await reqCourseWork[0];
 
 
+                        console.log('NEWNEW----------------')
+                        if (!storedCourseWork) storedCourseWork = await reqCourseWork;
 
-                        // if the course list length changed
-                    } else {
-                        // if the scanned course length is greater than stored
-                        if (await courseActs.length > await storedCoursesActivities.length) {
-                            console.log('There might be a new course added!'.toUpperCase());
-                            storedCoursesActivities = courseActs;
-                            await callSendAPI(await this.sender_psid, { text: `A new course has been added! Named: '${await courseActs[0].course}'` });
-                        }
-                        // if the scanned course is less than stored
-                        else if (await courseActs.length < await storedCoursesActivities.length) {
-                            console.log(`A class has been removed`.toUpperCase());
-
-                            // find the removed course activity
-                            const removedActivity = await storedCoursesActivities.find(async (activity) => await !courseActs.includes(await activity));
-
-                            // log and send a message about the removed course activity
-                            console.log(`Removed activity: ${await removedActivity}`);
-                            await callSendAPI(await this.sender_psid, { text: `A class has been removed: ${await removedActivity.course}` });
-
-                            // update the stored courses activities array
-                            storedCoursesActivities = await courseActs;
+                        if (await reqCourseWork.length === storedCourseList.length) {
+                            console.log("REQ:")
+                            console.log(reqCourseWork);
+                            console.log("STORED:");
+                            console.log(storedCourseWork);
                         } else {
-                            console.log("IDK man")
+                            console.log('NOTIFIED!!!')
                         }
-                    }
+                    })
+
+                } else if (await storedCourseList.length < await courseList.length) {
+                    //console.log('ADDED');
+                    //await callSendAPI(await this.sender_psid, { text: 'A new course has been added!' })
+                } else if (await storedCourseList.length > await courseList.length) {
+                    //console.log('REMOVED');
+                    //await callSendAPI(await this.sender_psid, { text: 'A course has been removed!' })
                 }
+
+                storedCourseList = courseList
             }
 
-            /*
-            await courses.forEach(async course => {
-                console.log("LISTENING...");
-                console.log("--------------------------")
-                const courseWork = await this.getCourseWork(await this.token, course.id).then(res => res);
-                console.log(await courseWork.map(work => work.title))
-                console.log("--------------------------")
-            })
-            */
-
-
-
+            //console.log(await courses.map(course => course.name));
+            //console.log(courseWorksObj.map(courseWork => courseWork.coursework.map(work => work.title)));
+            console.log('---------------------------')
 
         }, 1000);
 
     }
+
+    async listenCourseChange() {
+        const auth = new google.auth.OAuth2(
+            CLIENT_ID,
+            CLIENT_SECRET,
+            REDIRECT_URI
+        );
+
+        auth.setCredentials({
+            // Replace the following with your own values
+            access_token: this.token.access_token,
+            refresh_token: this.token.refresh_token
+        });
+
+        const classroom = google.classroom({
+            version: 'v1',
+            auth: auth
+        });
+
+        let courses = [];
+        let firstTime = true;
+
+        console.log('scanning...')
+
+        // Check for changes every 5 minutes
+        // Check for changes every 5 minutes
+        setInterval(() => {
+            classroom.courses.list({
+                courseStates: ['ACTIVE']
+            }, (err, res) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                const currentCourses = res.data.courses;
+
+                // Check for new added courses
+                if (!firstTime) {
+                    const newCourses = currentCourses.filter((course) => {
+                        return !courses.some((c) => c.id === course.id);
+                    });
+                    newCourses.forEach(async (course) => {
+
+
+                        const response = {
+                            attachment: {
+                                type: "template",
+                                payload: {
+                                    template_type: "button",
+                                    text: `New course added'${course.name}'`,
+                                    buttons: [
+                                        {
+                                            type: "web_url",
+                                            url: course.alternateLink,
+                                            title: `Go to New Course`,
+                                            webview_height_ratio: "full",
+                                        },
+                                    ],
+                                },
+                            },
+                        };
+
+                        console.log(`New course added: ${course.name}`);
+                        await callSendAPI(this.sender_psid, response)
+                    });
+                }
+
+                // Check for removed courses
+                const removedCourses = courses.filter((course) => {
+                    return !currentCourses.some((c) => c.id === course.id);
+                });
+                removedCourses.forEach(async (course) => {
+                    console.log(`Course removed: ${course.name}`);
+                    await callSendAPI(this.sender_psid, { text: 'Course removed: ' + course.name });
+                });
+
+                // Update courses list
+                courses = currentCourses;
+
+                if (firstTime) {
+                    firstTime = false;
+                }
+            });
+        }, 2000);
+    }
+
+    async hasWorkBeenSubmitted(courseId, courseWorkId, userId, auth) {
+        const res = await classroom.courses.courseWork.studentSubmissions.list({
+            auth: auth,
+            courseId: courseId,
+            courseWorkId: courseWorkId,
+            userId: userId
+        });
+
+        // Check if the list of submissions is not empty
+        return res.data.studentSubmissions.length > 0;
+    }
+
+    async getCourseWorks(token, courseId) {
+        const oauth2Client = new OAuth2Client(
+            CLIENT_ID,
+            CLIENT_SECRET,
+            REDIRECT_URI
+        )
+
+        oauth2Client.setCredentials({
+            access_token: token.access_token,
+            refresh_token: token.refresh_token
+        });
+
+        const classroom = google.classroom({
+            version: "v1",
+            auth: oauth2Client,
+        });
+
+        return new Promise(async (resolve, reject) => {
+            classroom.courses.courseWork.list({
+                auth: oauth2Client,
+                courseId: courseId
+            }, async (err, res) => {
+                if (err) {
+                    reject(err)
+                }
+
+                const courseWorks = await res.data.courseWork;
+
+                resolve(await courseWorks);
+            });
+        })
+    }
+
+
 
     async getCourses(token) {
 
@@ -142,7 +243,6 @@ class CourseListener {
             REDIRECT_URI
         )
 
-        // Replace YOUR_ACCESS_TOKEN and YOUR_REFRESH_TOKEN with your own values
         oauth2Client.setCredentials({
             access_token: token.access_token,
             refresh_token: token.refresh_token
@@ -164,69 +264,21 @@ class CourseListener {
                 const courses = res.data.courses;
 
                 resolve(courses);
-                /**
-                 * 
-                 * if (courses.length) {
-                    console.log('Courses:');
-                    courses.forEach(course => {
-                        console.log(`${course.name} (${course.id})`);
-                    });
-                } else {
-                    console.log('No courses found.');
-                }
-                 */
             });
         })
 
 
     }
 
-
-    async getCourseWork(token, courseId) {
-        const oauth2Client = new google.auth.OAuth2(
-            CLIENT_ID,
-            CLIENT_SECRET,
-            REDIRECT_URI
-        );
-
-        oauth2Client.setCredentials({
-            access_token: token.access_token,
-            refresh_token: token.refresh_token,
-        });
-
-        const classroom = google.classroom({
-            version: "v1",
-            auth: oauth2Client,
-        });
-
-        return new Promise((resolve, reject) => {
-            classroom.courses.courseWork.list(
-                {
-                    courseId: courseId,
-                },
-                (err, res) => {
-                    if (err) {
-                        console.error(err);
-                        reject(err);
-                    } else {
-                        const courseWork = res.data.courseWork;
-                        resolve(courseWork);
-                    }
-                }
-            );
-        });
-    }
-
-
-
 }
+
 
 async function getUsers() {
     db.once('open', async () => {
         const users = await db.collection('noteyfi_users').find().toArray((err, res) => res);
 
         users.forEach(user => {
-            new CourseListener(user).listen()
+            new CourseListener(user).listenCourseChange()
         })
 
     })
@@ -238,7 +290,7 @@ app.listen(PORT, console.log('Server is listening to port ' + PORT))
 
 
 // Sends response messages via the Send API
-function callSedAPI(sender_psid, response) {
+function callSendAPI(sender_psid, response) {
     // Construct the message body
     let request_body = {
         recipient: {
