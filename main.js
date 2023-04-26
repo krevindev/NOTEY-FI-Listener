8bc6cfde8b71c2ab8b65b5972cd107930da12aed
Glitch (glitch-hello-website) <none>
2023-04-26 16:08:16 +0000

    👱🐚 Checkpoint ./main.js:56626904/4023

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ const request = require('request'),
   axios = require('axios')
 
 app.use(express.json()); // Enable JSON request body parsing
+const CourseListener = require('./CourseListener').CourseListener
+
+app.post('/pass_data', (req, res) => {
+  const user = req.body;
+
+  new CourseListener(user).listenCourseChange();
+  new CourseListener(user).pushNotification();
+
+  console.log("RECEIVED DATA: ")
+  console.log(req.body)
+  res.status(200).send('Notification received')
+})
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('listener server is running'))
