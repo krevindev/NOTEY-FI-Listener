2c234f508d5cfc56acf5a95bfcc01b87ea3c27cb
Glitch (glitch-hello-website) <none>
2023-04-27 09:26:03 +0000

    💘🐟 Checkpoint ./CourseListener.js:56626904/1

--- CourseListener.js
+++ CourseListener.js
@@ -2,7 +2,6 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const SCOPES = process.env.SCOPE_STRING;
-
 const { google } = require('googleapis')
 
 const express = require('express')
